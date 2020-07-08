087086804e52d49e1d63118cedad93c6db6ae58f
DaOneRom <davepmoreno@gmail.com>
2020-07-08 14:50:34 +0800

    Home and NotFound Component

--- src/App.js
+++ src/App.js
@@ -1,98 +1,30 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
 } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-// import axios from 'axios';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
-	// const [users, setUsers] = useState([]),
-	// [user, setUser] = useState({}),
-	// [repos, setRepos] = useState([]),
-	// [loading, setLoading] = useState(false),
-	// [alert, setAlert] = useState(null);
-
-	// const searchUsers = async (text) => {
-	// 	setLoading(true);
-
-	// 	const res = await axios.get(
-	// 		`http://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-	// 	);
-
-	// 	setUsers(res.data.items);
-	// 	setLoading(false);
-	// };
-
-	// const getUser = async (username) => {
-	// 	setLoading(true);
-	// 	const res = await axios.get(
-	// 		`http://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-	// 	);
-	// 	setUser(res.data);
-	// 	setLoading(false);
-	// };
-
-	// const getUserRepos = async (username) => {
-	// 	setLoading(true);
-	// 	const res = await axios.get(
-	// 		`http://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-	// 	);
-	// 	setRepos(res.data);
-	// 	setLoading(false);
-	// };
-
-	// const clearUsers = () => {
-	// 	setUsers([]);
-	// 	setLoading(false);
-	// };
-
-	// 	const showAlert = (msg, type) => {
-	// 	setAlert({ msg, type });
-	// 	setTimeout(() => setAlert(null), 5000);
-	// };
-
 	return (
 		<GithubState>
 			<AlertState>
 				<Router>
 					<div className='App'>
-						<Navbar
-						// title='Github Finder'
-						// icon='fab fa-github'
-						/>
+						<Navbar />
 						<div className='container'>
 							<Alert />
 							<Switch>
-								<Route
-									exact
-									path='/'
-									render={(props) => (
-										<Fragment>
-											<Search
-											// searchUsers={searchUsers}
-											// clearUsers={clearUsers}
-											// showClear={
-											// 	users.length > 0 ? true : false
-											// }
-											// setAlert={showAlert}
-											/>
-											<Users
-											// loading={loading}
-											// users={users}
-											/>
-										</Fragment>
-									)}
-								/>
+								<Route exact path='/' component={Home} />
 								<Route
 									exact
 									path='/about'
@@ -102,17 +34,8 @@ const App = () => {
 									exact
 									path='/user/:login'
 									component={User}
-									// render={(props) => (
-									// <User
-									// {...props}
-									// getUserRepos={getUserRepos}
-									// repos={repos}
-									// getUser={getUser}
-									// loading={loading}
-									// user={user}
-									// />
-									// )}
 								/>
+								<Route component={NotFound} />
 							</Switch>
 						</div>
 					</div>
